61929f4520fe4610313329c166d4565cf7da4c01
Surya Selvakumar <surya@192.168.1.228>
2026-03-29 19:52:12 +0100

    new-address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,10 @@ export default function Footer() {
     <footer className={isHomePage ? 'footer home-footer' : 'footer'}>
       <p>&copy; {currentYear} NovaHealZ. All rights reserved.</p>
       {/* <p>Contact: <a href="mailto:info@healtech.com">info@novahealz.com</a></p> */}
-      {/* <p>272 Bath Street, Glasgow, G2 4JR, Scotland</p>
+      <p>Suite 2/3, 2nd Floor, 48 West George Street</p>
+      <p>Glasgow, UK</p>
       <p>Company number: SC878319</p>
-      <p>Registered in Scotland</p> */}
+      <p>Registered in Scotland</p>
       
     </footer>
   );
